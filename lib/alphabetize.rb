712de178253348c6714171ac2a2263592ca1dcb5
Sarah Evans <sarah.g.evans42@gmail.com>
2017-05-23 19:06:52 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  arr.sort_by { |phrase| phrase.chars.map { |char| esperanto_alphabet.index(char)}}
 end
